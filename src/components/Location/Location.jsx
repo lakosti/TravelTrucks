53aa7c5cadddefc854cd7d4b97decbf5bfa9cd7e
Yaroslava <yaroslava.hovenko@gmail.com>
2024-09-09 18:28:32 +0300

    add location form style

--- src/components/Location/Location.jsx
+++ src/components/Location/Location.jsx
@@ -5,13 +5,21 @@ import css from "./Location.module.css";
 const Location = () => {
   return (
     <>
-      <h2 className={css.subtitle}>Location</h2>
-      <div className={css.position}>
-        <input type="text" placeholder="Enter location" className={css.input} />
-        <svg width={20} height={20} className={css.icon}>
-          <use href={`${icons}#map`} />
-        </svg>
-      </div>
+      <form className={css.form}>
+        <label className={css.label}>
+          <span className={css.location}>Location</span>
+          <div className={css.position}>
+            <input
+              type="text"
+              placeholder="Kyiv, Ukraine"
+              className={css.input}
+            />
+            <svg width={20} height={20} className={css.icon}>
+              <use href={`${icons}#map`} />
+            </svg>
+          </div>
+        </label>
+      </form>
     </>
   );
 };
